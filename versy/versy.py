c842f424f0d15c2cc9cf00a422e340f6373aeda0
Tom Ritchford <tom@swirly.com>
2020-07-22 20:57:36 +0200

    Version v0.9.2

--- versy/versy.py
+++ versy/versy.py
@@ -12,7 +12,7 @@ import safer
 PREFIX = '__version__ = '
 VERSION = 'VERSION'
 ACTIONS = 'patch', 'minor', 'major', 'new', 'show'
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 
 def versy(action, changelog, dry_run, message, path, verbose, edit):
