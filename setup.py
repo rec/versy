9bbf8d07b5562aa94649aab9365dc370e72ced30
Tom Ritchford <tom@swirly.com>
2020-11-07 13:21:47 +0100

    Update setup.py to Production/Stable

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 _classifiers = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
